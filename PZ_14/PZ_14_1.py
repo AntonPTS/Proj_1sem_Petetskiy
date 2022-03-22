42431487a6f92d988c221a1ae50dc028c1d4c6a9
Anton <antohapetetskij@gmail.com>
2022-03-22 21:37:06 +0300

    PZ_14

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -4,36 +4,11 @@
 
 import re
 
-p = re.compile(r'[0-9]')
 with open('experience.txt', 'r', encoding='utf-8') as exp:
     text = exp.read()
-    a = re.findall(r'\d+', text)      # findall - поиск всех совпадений в шаблоне
-l1 = []
-i = 0
-b = len(a)
-while i < b:
-    l1.append(int(a[i]))
-    i += 2
-p = " ".join(map(str, l1))      # map - применяет функцию к каждому элементу итерируемого объекта
-o = p.split(' ')      # split - возвращает список слов в строке
-l2 = []
-n = 1
-while n < b:
-    l2.append(int(a[n]))
-    n += 2
-p1 = " ".join(map(str, l2))
-o1 = p1.split(' ')
-s = [x * 12 for x in l1]        # списковое включение
-p2 = " ".join(map(str, s))
-o2 = p2.split(' ')
-s2 = []
-for t in range(len(l2)):
-    g1 = s[t] + l2[t]
-    s2.append(g1)
-else:
-    s2.append(s[len(s)-1])
-p3 = ", ".join(map(str, s2))
-o3 = p3.split(', ')
-print(f'Стажи работы (в месяцах): {p3}')
-k = len(o3)
-print(f'Количество стажей работы: {k}')
+    a = re.findall(r'\b[0-9]+.\w+.[0-9]+.\w*', text)      # findall - поиск всех совпадений в шаблоне
+    b = re.findall(r'\b3+.\w{4}\b', text)
+    a.append(b[0])
+    c = ", ".join(map(str, a))     # map - применяет функцию к каждому элементу итерируемого объекта
+    print('Стажи работы: ', c)
+    print('Количество стажей работы: ', len(a))
