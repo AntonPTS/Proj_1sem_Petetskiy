86fdae517ef057d0458831c5b3b8b24b24c8df07
Anton <antohapetetskij@gmail.com>
2022-03-23 00:07:42 +0300

    module

---
+++ module/__init__.py
@@ -0,0 +1,5 @@
+from .lesson_set import *
+from .lesson_file import *
+from .doc import *
+
+NAME = "курс лекций по Python"
---
+++ module/doc/file_doc.py
@@ -0,0 +1,3 @@
+from ..lesson_file import get_binary
+
+doc = """Раздаточный материал по файлам: """ + get_binary()
